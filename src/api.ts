3e36ab3436933f2d085db68cc9bd7b9320a3f36f
linux_china <libing.chen@gmail.com>
2026-08-06 12:18:46 +0800

    chore: add dot2svg

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,14 @@
 import {check} from "./checker";
 import {parse} from "./parser";
 import {Format, render} from "./renderer";
+import {instance} from "@viz-js/viz";
 
-export async function renderDbml(input: string, format: Format): Promise<string> {
-    return await render(check(parse(input)), format);
+const vizPromise = instance();
+
+export async function renderDbml(dbmlText: string, format: Format): Promise<string> {
+    return await render(check(parse(dbmlText)), format);
+}
+
+export async function dot2svg(dotText: string) {
+    return (await vizPromise).renderString(dotText, {format: "svg"});
 }
